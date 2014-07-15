d61867f7b0f75f43a05860127a00c5137e4db717
agustinorigone <agustin.origone@gmail.com>
2014-07-14 19:51:30 -0700

    .

--- views/navigation/banner.php
+++ views/navigation/banner.php
@@ -2,12 +2,12 @@
 $club = getInput("club");
 ?>
 <div class="banner">
-	<div class="bannerContainer right">
-		<p class ="bannerTitle"><?php echo $club['name']?></p>
-		<p class ="bannerSubtitle"><?php echo $club['neighbour']?></p>
+	<div class="bannerTitle">
+		<p><?php echo $club['name']?></p>
+		<p><?php echo $club['neighbour']?></p>
 	</div>
 	<img src='images/<?php echo $club['image']?>'></img>
-	<div class="bannerContainer left">
+	<div class="bannerDescription">
 		<p><?php echo "Email: ".$club['email']?></p>
 		<p><?php echo "Ubicaci&oacuten: ".$club['location']?></p>
 		<p><?php echo "Tel: ".$club['phone']?></p>
--- views/navigation/header.php
+++ views/navigation/header.php
@@ -1,9 +1,9 @@
 <?php echo loadView('navigation/htmlHeader'); ?>
 <div class='zammerContainer'>
 	<div class='zammerLogo'>
-		<img src="images/logo_zammer.png"></img>
+		<img src="images/logo_zammer_color_medium.png"></img>
 	</div>
 	<div class='zammerText'>
-		<img src="images/texto_zammer.png"></img>
+		<img src="images/texto_zammer_color.png"></img>
 	</div>
 </div>
