69774feeaa0d9d0d6e42323a24e4552c03d72a8a
agustinorigone <agustin.origone@gmail.com>
2014-07-14 22:10:36 -0700

    add carousel

--- views/navigation/htmlHeader.php
+++ views/navigation/htmlHeader.php
@@ -1,3 +1,5 @@
 <link rel="stylesheet" type="text/css" href="views/style.css">
+<link rel="stylesheet" type="text/css" href="views/carousel.css">
 <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<script type="text/javascript" src="js/jquery.flexisel.js"></script>
--- views/proshop/index.php
+++ views/proshop/index.php
@@ -2,13 +2,50 @@
 $products = getInput("products");
 ?>
 <div class="proshopContainer">
-<?php foreach ($products as $product){?>
-	<div class = "product">
-		<img title="<?php echo $product->attributes->name?>" src="<?php echo "images/".$product->attributes->image?>"></img>
-		<div class = "productDescription">
-			<a><?php echo $product->attributes->name?></a>
-			<p><?php echo "$".$product->attributes->price?></p>
-		</div>
+	<div>
+		<ul id="carousel"> 
+		    <li><img src="images/carousel/logo_zammer.png" /></li>
+		    <li><img src="images/carousel/logo_head.png" /></li>    
+		    <li><img src="images/carousel/logo_vairo.jpg" /></li>     
+		    <li><img src="images/carousel/logo_zammer.png" /></li> 
+		    <li><img src="images/carousel/logo_head.png" /></li>                                                          
+		</ul>
 	</div>
-<?php }?>
+	
+	<div>
+	<?php foreach ($products as $product){?>
+		<div class = "product">
+			<img title="<?php echo $product->attributes->name?>" src="<?php echo "images/".$product->attributes->image?>"></img>
+			<div class = "productDescription">
+				<a><?php echo $product->attributes->name?></a>
+				<p><?php echo "$".$product->attributes->price?></p>
+			</div>
+		</div>
+	<?php }?>
+	</div>
 </div>
+<script type="text/javascript">
+	$("#carousel").flexisel({
+        visibleItems: 5,
+        animationSpeed: 1000,
+        autoPlay: true,
+        autoPlaySpeed: 3000,            
+        pauseOnHover: true,
+        enableResponsiveBreakpoints: true,
+        responsiveBreakpoints: { 
+            portrait: { 
+                changePoint:480,
+                visibleItems: 1
+            }, 
+            landscape: { 
+                changePoint:640,
+                visibleItems: 2
+            },
+            tablet: { 
+                changePoint:768,
+                visibleItems: 3
+            }
+        }
+    });    
+});
+</script>
